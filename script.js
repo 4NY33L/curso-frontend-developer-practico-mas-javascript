c14db4ee584816d0b67f6c9660113603080487de
Anyel Ortega <anyelpertuz@gmail.com>
2025-04-11 10:30:15 -0500

    Integré el aside del carrito de compras en el index.html y programé con JavaScript que aparezca o desaparezca cuando le den click al icono del carrito de compras que está en la navbar. Tambien programé que cuando le den click a dicho icono, se le aplique la clase inactive  (en caso de que la tenga) al menú mobile, para que no salgan ambos juntos. También le apliqué lo mismo aside del carrito de compras.

--- script.js
+++ script.js
@@ -2,15 +2,34 @@ const navbarEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
 const burgerMenu = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
+const asideCartDetail = document.querySelector(".cart-detail") 
+const iconShoppingCart = document.querySelector(".navbar-shopping-cart")
 
 navbarEmail.addEventListener("click", toggleDisplayDesktopMenu)
 burgerMenu.addEventListener("click", toggleDisplayMobileMenu)
+iconShoppingCart.addEventListener("click", toggleDisplayAsideCartDetail)
 
 
 function toggleDisplayDesktopMenu() {
+    if (!asideCartDetail.classList.contains("inactive")) {
+        asideCartDetail.classList.add("inactive")
+    }
     desktopMenu.classList.toggle("inactive")
 }
 
 function toggleDisplayMobileMenu() {
+    if (!asideCartDetail.classList.contains("inactive")) {
+        asideCartDetail.classList.add("inactive")
+    }
     mobileMenu.classList.toggle("inactive")
+}
+
+function toggleDisplayAsideCartDetail() {
+    if (!mobileMenu.classList.contains("inactive")) {
+        mobileMenu.classList.add("inactive")
+    }
+    if (!desktopMenu.classList.contains("inactive")) {
+        desktopMenu.classList.add("inactive")
+    }
+    asideCartDetail.classList.toggle("inactive")
 }
