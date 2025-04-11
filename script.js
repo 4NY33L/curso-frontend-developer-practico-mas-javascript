65a5806c0be9b87e47885b334c15edbb7ffaae2e
Anyel Ortega <anyelpertuz@gmail.com>
2025-04-11 12:31:40 -0500

    Integré el main (osea la lista de productos) al index.html. Quité la información hardcodeada de las cards de los productos que estaba en el html y creé la estructura de card del producto con un template string desde JavaScript para insertar cada producto de forma dinámica en el html. Ovbiamente, La información ahora está hardcodeada en JavaScript pero eso se soluciona cuando conectemos el frontend con el backend mediante una API para traer información y construir los productos en base a ella. Utilicé el  ciclo 'for (elemento of array)' para recorrer la lista de productos.

--- script.js
+++ script.js
@@ -4,11 +4,45 @@ const burgerMenu = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
 const asideCartDetail = document.querySelector(".cart-detail") 
 const iconShoppingCart = document.querySelector(".navbar-shopping-cart")
+const productCardContainer = document.querySelector(".cards-container")
+const productList = [
+    {
+        name: "Audífonos",
+        price: 35,
+        img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    },
+    {
+        name: "Silla Gamer",
+        price: 400,
+        img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    },
+    {
+        name: "Audífonos",
+        price: 35,
+        img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    },
+    {
+        name: "Audífonos",
+        price: 35,
+        img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    },
+    {
+        name: "Audífonos",
+        price: 35,
+        img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    },
+    {
+        name: "Audífonos",
+        price: 35,
+        img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    }
+]
 
 navbarEmail.addEventListener("click", toggleDisplayDesktopMenu)
 burgerMenu.addEventListener("click", toggleDisplayMobileMenu)
 iconShoppingCart.addEventListener("click", toggleDisplayAsideCartDetail)
 
+renderProducts(productList)
 
 function toggleDisplayDesktopMenu() {
     if (!asideCartDetail.classList.contains("inactive")) {
@@ -32,4 +66,23 @@ function toggleDisplayAsideCartDetail() {
         desktopMenu.classList.add("inactive")
     }
     asideCartDetail.classList.toggle("inactive")
+}
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = `<div class="product-card">
+                <img src=${product.img}
+                    alt=${product.name}>
+                <div class="product-info">
+                    <div>
+                        <p>$${product.price}</p>
+                        <p>${product.name}</p>
+                    </div>
+                    <figure>
+                        <img src="./icons/bt_add_to_cart.svg" alt="">
+                    </figure>
+                </div>
+            </div>`
+        productCardContainer.insertAdjacentHTML("beforeend", productCard)
+    }
 }
