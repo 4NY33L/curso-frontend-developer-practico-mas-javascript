2be41924b5f924447db26da018d148abc3545c09
Anyel Ortega <anyelpertuz@gmail.com>
2025-04-15 09:30:19 -0500

    1. Programé con JavaScript que cuando se dé click en alguna card de un producto salga el aside que tiene los detalles del producto correspondiente, esto lo hice recorriendo la lista de pruductos para encontrar el que tuviera el mismo id que el input checkeado y creando un template string del product detail en base al producto correspondiente. Para ello, tuve que modificar la estructura html interna del aside, cambiando el <div> que representa la card por un <label> seguido de un <input> de tipo radio relacionado con el <label>.
    Razones por las cuales coloqué el  input después del label y no dentro, y por qué le escuché el evento click al input y no al label:
    - Porque los <input> deberían ir a la misma altura de los label.
    - Como al principio era el label el que tenía el evento click, si colocaba el input adentro iba a generar problemas de propagación de eventos, ya que cuando seleccionamos un input dándole click a él directamente o dándole click a su label asociado esto dispara su evento de click, por ende, la función de mostrar el aside se ejecutaría dos veces, cuando cliqueara el label y cuando se checkeara automaticamente el input.
    - Le agregué el escuchador de eventos al input en vez del label porque la función del evento click del label se ejecutaba antes de que el input se marcara como checkeado, por lo que cuando intentaba verificar qué input estaba checkeado no me aparecía ninguno. De esta manera, cuando le demos click al label ya no ejecutaremos ninguna función directamente sino que esperaremos que el input asociado a ese label se marque automaticamente como checkeado, lo cual dispara su evento click y ahí sí se ejecuta nuestra función de mostrar aside del product detail.
    
    Otros inconvenientes que tuve pero pude solucionar:
    
    - En la función renderProducts(), el ciclo 'for (product of arr)' que recorre la lista de productos para crear las cards dinamicamente me generó un problemita jodón en primera instancia, debido a que no especifiqué el tipo de variable que sería product, JavaScript la estaba creando como variable global (con var), lo cual generó conflictos con otra variable que tiene el mismo nombre en otra función. Esto lo solucioné declarando product con let para que solo existiera en esa función, así: for (let product of arr).

--- script.js
+++ script.js
@@ -2,38 +2,45 @@ const navbarEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
 const burgerMenu = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
-const asideCartDetail = document.querySelector(".cart-detail") 
+const asideCartDetail = document.querySelector(".cart-detail")
 const iconShoppingCart = document.querySelector(".navbar-shopping-cart")
 const productCardContainer = document.querySelector(".cards-container")
+const asideProductDetail = document.querySelector(".product-detail")
 const productList = [
     {
+        id: 1,
         name: "Audífonos",
         price: 35,
         img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
     },
     {
+        id: 2,
         name: "Silla Gamer",
         price: 400,
         img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
     },
     {
-        name: "Audífonos",
-        price: 35,
+        id: 3,
+        name: "Oversize",
+        price: 60,
         img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
     },
     {
-        name: "Audífonos",
-        price: 35,
+        id: 4,
+        name: "Pantaloneta",
+        price: 20,
         img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
     },
     {
-        name: "Audífonos",
-        price: 35,
+        id: 5,
+        name: "Gorra RedBull Batalla",
+        price: 90,
         img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
     },
     {
-        name: "Audífonos",
-        price: 35,
+        id: 6,
+        name: "Jordan Retro one",
+        price: 150,
         img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
     }
 ]
@@ -43,6 +50,11 @@ burgerMenu.addEventListener("click", toggleDisplayMobileMenu)
 iconShoppingCart.addEventListener("click", toggleDisplayAsideCartDetail)
 
 renderProducts(productList)
+
+const inputsProduct = [...document.querySelectorAll(".cards-container input")]
+inputsProduct.forEach((input) => {
+    input.addEventListener("click", showProductDetail)
+})
 
 function toggleDisplayDesktopMenu() {
     if (!asideCartDetail.classList.contains("inactive")) {
@@ -65,12 +77,15 @@ function toggleDisplayAsideCartDetail() {
     if (!desktopMenu.classList.contains("inactive")) {
         desktopMenu.classList.add("inactive")
     }
+    if (!asideProductDetail.classList.contains("inactive")) {
+        asideProductDetail.classList.add("inactive")
+    }
     asideCartDetail.classList.toggle("inactive")
 }
 
 function renderProducts(arr) {
-    for (product of arr) {
-        const productCard = `<div class="product-card">
+    for (let product of arr) {
+        const productCard = `<label for=${product.id} class="product-card">
                 <img src=${product.img}
                     alt=${product.name}>
                 <div class="product-info">
@@ -81,8 +96,39 @@ function renderProducts(arr) {
                     <figure>
                         <img src="./icons/bt_add_to_cart.svg" alt="">
                     </figure>
-                </div>
-            </div>`
+                </div>                
+            </label>
+            <input type="radio" id=${product.id} name="product" style="display: none">`
+
         productCardContainer.insertAdjacentHTML("beforeend", productCard)
     }
+}
+
+function showProductDetail(ev) {
+    asideProductDetail.classList.remove("inactive")
+    const inputChecked = ev.target
+    console.log(inputChecked.checked)
+    const product = productList.find(product => product.id === +inputChecked.id)
+
+    const templateProductDetail = `
+        <div class="product-detail-close">
+            <img src="./icons/icon_close.png" alt="close">
+        </div>
+        <img src=${product.img}>
+        <div class="product-info">
+            <p>$${product.price}</p>
+            <p>${product.name}</p>
+            <p>With its practical position, this bike also fulfills a decorative function, add your hall or workspace.</p>
+            <button class="primary-button add-to-cart-button">
+                <img src="./icons/bt_add_to_cart.svg" alt="add to cart">
+                Add to cart
+            </button>
+        </div>`
+    asideProductDetail.innerHTML = templateProductDetail
+    const productDetailClose = document.querySelector(".product-detail-close")
+    productDetailClose.addEventListener("click", closeAsideProductDetail)
+}
+
+function closeAsideProductDetail() {
+    asideProductDetail.classList.add("inactive")
 }
